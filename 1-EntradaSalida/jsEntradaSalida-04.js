9792643ada7e80d1e5494828d536403c15c9c71b
bellagambacecilia99@gmail.com <ceciliabellagamba>
2021-07-13 22:04:59 -0300

    nuevo cambio

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,15 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reservo espacio en memoria para guardar el nombre del usuario
+	var nombre;
+	
+  //guardo en la variable nombre el texto que escribio el usuario dentro de la ventana prompt
+	nombre = prompt("Ingrese su nombre");
+
+	//copio el nombre que tengo guardado en la variable nombre dentro de la caja de texto embebida en la página html
+	document.getElementById("txtIdNombre").value = nombre;
+	
+
 }
 
